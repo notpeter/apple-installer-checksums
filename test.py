295ecdcc4567a755855f7d0d4662b695219bf353
Peter Tripp <petertripp@gmail.com>
2017-04-09 03:38:58 -0700

    Single CSV. pep8, pyflakes.

--- test.py
+++ test.py
@@ -9,13 +9,14 @@ from tempfile import NamedTemporaryFile
 # Would be great if we could use subtests and not load the JSON each time
 # Unittest subtests require Python >3.4 or unittest2 (backport)
 
+
 class AppleChecksumsJSON(unittest.TestCase):
-    def test_json(self):
+    def test_json1(self):
         """apple_checksums.json and is actually json"""
         with open('apple_checksums.json') as file:
             json.load(file)
 
-    def test_json(self):
+    def test_json2(self):
         """apple_checksums.json is json"""
         with open('apple_checksums.schema.json') as file:
             json.load(file)
@@ -34,12 +35,17 @@ class AppleChecksumsJSON(unittest.TestCase):
         with open('apple_checksums.json') as file:
             file_json = json.load(file)
         with NamedTemporaryFile(mode='w') as tmp:
-            json.dump(file_json, tmp, indent=4, sort_keys=True, separators=(',', ': '))
+            json.dump(file_json, tmp, indent=4, separators=(',', ': '),
+                      separators=(',', ': '))
             # add final newline (json.dump exludes it)
             tmp.write('\n')
             tmp.flush()
-            ret = subprocess.call(['diff', '-U5', 'apple_checksums.json', tmp.name])
-            self.assertEqual(ret, 0, "JSON doesn't match expected sort and formatting.")
+            # TODO: Use pythonic diffing instead of just calling 'diff' util
+            diff_cmd = ['diff', '-U5', 'apple_checksums.json', tmp.name]
+            ret = subprocess.call(diff_cmd)
+            self.assertEqual(ret, 0, "JSON doesn't match expected sort and "
+                                     "formatting. Maybe run update.py to "
+                                     "fancy up your json.")
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
--- update.py
+++ update.py
@@ -3,34 +3,32 @@ import csv
 import json
 
 
-def dump_fancy(obj, fp):
-    json.dump(obj, fp, indent=4, sort_keys=True, separators=(',', ': '))
-    # add final newline (json.dump exludes it)
-    fp.write('\n')
-
 def pretty_json(filename):
     """Overwrites a JSON file with a pretty copy. Indents and sorts keys."""
     with open(filename) as file:
         data = json.load(file)
     with open(filename, 'w') as file:
-        dump_fancy(data, file)
+        # Git friendly "predictable" JSON, sorted, standardized whitespace.
+        json.dump(data, file, indent=4, sort_keys=True, separators=(',', ': '))
+        # add final newline (cause json.dump exludes it)
+        file.write('\n')
+
 
 def update():
     pretty_json('apple_checksums.json')
     with open('apple_checksums.json') as acj:
         checksums = json.load(acj)
     # Fancy method, but no do custom orders.
-    #columns = set().union(*(d.keys() for d in checksums))
-    columns = ['product', 'version', 'build', 'sha1', 'sha256', 'filename', 'note', 'url']
-    for filename, match in {"macos_checksums.csv": "Mac",
-                            "xcode_checksums.csv": "Xcode",
-                            "apple_checksums.csv": ""}.items():
-        with open(filename, 'w') as csv_file:
-            writer = csv.DictWriter(csv_file, fieldnames=columns)
-            writer.writeheader()
-            for apple_file in checksums:
-                if match in apple_file['product']:
-                    writer.writerow(apple_file)
+    # columns = set().union(*(d.keys() for d in checksums))
+    columns = ['product', 'version', 'build', 'sha1', 'sha256', 'filename',
+               'note', 'url']
+    # Write everything to the CSV
+    with open("apple_checksums.csv", 'w') as csv_file:
+        writer = csv.DictWriter(csv_file, fieldnames=columns)
+        writer.writeheader()
+        for apple_file in checksums:
+            writer.writerow(apple_file)
+
 
 if __name__ == '__main__':
     update()
